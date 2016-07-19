52aa9feae65f18e617435ad5e9032d5e06c5d45e
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2016-07-18 21:06:24 -0600

    Fix path for jupyter build.

---
+++ jupyter/install_packages.R
@@ -0,0 +1,5 @@
+# List of Packages to install
+pkglist <- c("ggplot2","caret","data.table","h2o","rgdal","gstat","rmarkdown","shiny","knitr","stringr","reshape2","plyr","lubridate","ncdf","mapproj","sp","RColorBrewer","gridExtra","twitteR","RMySQL","pushoverr")
+
+# Install listed packages
+lapply(pkglist,function(x){install.packages(x)})
