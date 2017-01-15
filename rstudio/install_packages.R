063479a7f38f1b69a4373267dd06ed2b5ea16488
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2017-01-15 16:16:06 -0700

    Add a few packages & pandoc

--- rstudio/install_packages.R
+++ rstudio/install_packages.R
@@ -1,5 +1,5 @@
 # List of Packages to install
-pkglist <- c("ggplot2","caret","data.table","h2o","rgdal","gstat","rmarkdown","shiny","knitr","stringr","reshape2","plyr","lubridate","ncdf","mapproj","sp","RColorBrewer","gridExtra","twitteR","RMySQL","pushoverr")
+pkglist <- c("ggplot2","caret","data.table","h2o","rgdal","gstat","rmarkdown","shiny","knitr","stringr","reshape2","plyr","lubridate","ncdf","mapproj","sp","RColorBrewer","gridExtra","twitteR","RMySQL","pushoverr","FinCal","documair","lintr","miniUI")
 
 # Install listed packages
 lapply(pkglist,function(x){install.packages(x)})
